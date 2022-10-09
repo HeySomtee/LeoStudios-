b0bfee34a19d10c92015da8b31fa2ecf60518526
unknown <ilosomto2000@gmail.com>
2022-10-09 18:31:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,228 @@
+// const win_dow = document.getElementById('container1')
+// function myFunction() {
+//     win_dow.scrollBy(00, 610);
+//     // alert("pageXOffset: " + win_dow.pageXOffset + ", pageYOffset: " + win_dow.pageYOffset);
+// }
+
+setInterval(function name() {
+    if (
+        document.getElementById('class').src = '2-1_-_Copy_-_Copy-removebg-preview.png'
+    ) {
+        document.getElementById('class').src = '3_-_Copy_-_Copy-removebg-preview.png'
+    } 
+    
+    
+} , 100)
+
+setInterval( function name1() {
+    if (
+        document.getElementById('class').src = '3_-_Copy_-_Copy-removebg-preview.png'
+    ) {
+        document.getElementById('class').src = 'IMG-20220929-WA0099-removebg-preview.png'
+    }
+} ,300)
+
+setInterval( function name2() {
+    if (
+        document.getElementById('class').src = 'IMG-20220929-WA0099-removebg-preview.png'
+    ) {
+        document.getElementById('class').src = 'staff_002_-_Copy-removebg-preview (1).png'
+    }
+} ,500)
+
+setInterval( function name3() {
+    if (
+        document.getElementById('class').src = 'staff_002_-_Copy-removebg-preview (1).png'    
+    ) {
+        document.getElementById('class').src = 'IMG-20220929-WA0098_-_Copy_-_Copy-removebg-preview.png'
+    }
+} ,700)
+
+setInterval( function name4() {
+    if (
+        document.getElementById('class').src = 'IMG-20220929-WA0098_-_Copy_-_Copy-removebg-preview.png'
+    ) {
+        document.getElementById('class').src = '007_-_Copy_-_Copy-removebg-preview.png'
+    }
+} ,900)
+
+setInterval( function name5() {
+    if (
+        document.getElementById('class').src = '007_-_Copy_-_Copy-removebg-preview.png'
+    ) {
+        document.getElementById('class').src = '001_-_Copy_-_Copy-removebg.png'
+    }
+} ,1100)
+
+setInterval( function name6() {
+    if (
+        document.getElementById('class').src = '001_-_Copy_-_Copy-removebg.png'
+    ) {
+        document.getElementById('class').src = 'IMG-20220929-WA0100_-_Copy-removebg-preview.png'
+    }
+} ,1300)
+
+// name()
+// name1()
+// name2()
+// name3()
+// name4()
+// name5()
+// name6()
+
+
+const shift = document.getElementById('fa');
+const shift1 = document.getElementById('sc1');
+const shift2 = document.getElementById('sc2');
+const shift3 = document.getElementById('sc3');
+const shift4 = document.getElementById('sc4');
+
+
+// shift.addEventListener('click', myFunction)
+const click_ = document.getElementById('onClick')
+const win_dow = document.getElementById('class')
+
+// click_.addEventListener('click', scrollY)
+// click_.addEventListener('click', scrollYY)
+function scrollY() {
+    document.getElementById('iconImg').src = document.getElementById('iconImg').src = '/twittericon.png'
+    // document.getElementById('ref').href = 'https://twitter.com/home'
+
+    if (
+        document.getElementById('iconImg').src = "/twittericon.png"
+    ) {
+        document.getElementById('ref').href = 'twitter.com'
+    }    
+}
+
+function scroll() {
+    win_dow.scrollBy(0, 10)
+}
+
+
+
+const icon_btn = document.getElementById('fa')
+icon_btn.addEventListener('click', icon)
+
+function icon(e) {
+    document.getElementById('dropdoen-content').style.display = 'block';
+    document.getElementById('fa').className = 'fa-solid fa-caret-up'
+
+    e.stopImmediatePropagation();
+    this.removeEventListener("click", icon);
+    var doc = document.getElementById('fa')
+    doc.onclick = icon1;
+}
+
+function icon1(e) {
+    document.getElementById('dropdoen-content').style.display = 'none';
+    document.getElementById('fa').className = 'fa-solid fa-caret-down'
+
+    e.stopImmediatePropagation();
+    this.removeEventListener("click", icon1);
+    var doc = document.getElementById('fa')
+    doc.onclick = icon;
+}
+
+function float() {
+    document.getElementById('background-wrap').style.display = 'none'
+    document.getElementById('background-wrap').style.pointerEvents = 'none'
+}
+
+const share_btn = document.getElementById('redirect');
+const recieve_btn =  document.getElementById('here');
+const recieve_btn1 =  document.getElementById('here1');
+const recieve_btn2 =  document.getElementById('here2');
+const switch_btn = document.getElementById('f')
+
+switch_btn.addEventListener('click', change)
+function change(e) {
+
+    share_btn.src = recieve_btn2.src
+    console.log(recieve_btn.src);
+    document.getElementById('a').href = 'https://twitter.com/LeoStudio_LS'
+
+    e.stopImmediatePropagation();
+                this.removeEventListener("click", change);
+                var doc = document.getElementById('f')
+                doc.onclick = change1;
+}
+
+function change1(e) {
+
+    share_btn.src = recieve_btn.src
+    console.log(share_btn.src);
+    document.getElementById('a').href = 'https://www.instagram.com/leo_studio666/'
+
+    e.stopImmediatePropagation();
+                this.removeEventListener("click", change1);
+                var doc = document.getElementById('f')
+                doc.onclick = change2;
+}
+
+function change2(e) {
+
+    share_btn.src = recieve_btn1.src
+    console.log(share_btn.src);
+    document.getElementById('a').href = 'https://discord.gg/leostudio'
+
+
+    e.stopImmediatePropagation();
+                this.removeEventListener("click", change2);
+                var doc = document.getElementById('f')
+                doc.onclick = change;
+}
+
+
+const clicker = document.getElementById('f1')
+const share_btnn = document.getElementById('redirect1');
+
+clicker.addEventListener('click', chang)
+function chang(e) {
+
+    share_btnn.src = recieve_btn2.src
+    console.log(recieve_btn.src);
+    document.getElementById('a1').href = 'https://twitter.com/LeoStudio_LS'
+
+    e.stopImmediatePropagation();
+                this.removeEventListener("click", chang);
+                var doc = document.getElementById('f1')
+                doc.onclick = chang1;
+}
+
+function chang1(e) {
+
+    share_btnn.src = recieve_btn.src
+    console.log(share_btn.src);
+    document.getElementById('a1').href = 'https://www.instagram.com/leo_studio666/'
+
+    e.stopImmediatePropagation();
+                this.removeEventListener("click", chang1);
+                var doc = document.getElementById('f1')
+                doc.onclick = chang2;
+}
+
+function chang2(e) {
+
+    share_btnn.src = recieve_btn1.src
+    console.log(share_btn.src);
+    document.getElementById('a1').href = 'https://discord.gg/leostudio'
+
+
+    e.stopImmediatePropagation();
+                this.removeEventListener("click", chang2);
+                var doc = document.getElementById('f1')
+                doc.onclick = chang;
+}
+const style_ = document.getElementById('soc_icons')
+shift4.addEventListener('click', sC)
+function sC() {
+    style_.style.display = 'block'
+    style_.style.animationName = 'example'
+}
+
+
+
+
+// float()
+// DO BUBBLES FOR SOCIAL ICONS
